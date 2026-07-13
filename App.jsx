a495d77535e8cc26bfd99ca4b2776a6984c88681
Priya_Shukla16 <priyashukla160106@gmail.com>
2026-07-14 01:14:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{fontSize:"40px", color:"red"}}>
+        @ps10 Project Running
+      </h1>
+
+      <p>
+        React is working successfully.
+      </p>
+    </div>
+  )
+}
+
+export default App
